6553021d260a4a804f7a4e29e38765d2d41dcca6
MADAN126 <madan20131312@gmail.com>
2026-05-21 09:41:47 +0530

    Create NetBankingDTO class for payment service

---
+++ NetBankingDTO.java
@@ -0,0 +1,13 @@
+package com.PaymentService.main.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class netBanking {
+	private String userId;
+
+	private String password;
+}
